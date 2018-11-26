32a8a0437ccee38d976fb95a22e5e966dfea5d3a
liuhui <liuhui@qianmi.com>
2018-11-26 18:03:52 +0800

    细节修改

--- src/main/java/com/cxxy/websocket/MyHandsHander.java
+++ src/main/java/com/cxxy/websocket/MyHandsHander.java
@@ -6,6 +6,7 @@ import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
+
 import java.nio.charset.Charset;
 import java.security.Principal;
 import java.util.Map;
@@ -17,7 +18,7 @@ import java.util.stream.Collectors;
  * Description:
  * Date: 5:03 PM 2018/11/26
  */
-public class MyHandsHander extends DefaultHandshakeHandler{
+public class MyHandsHander extends DefaultHandshakeHandler {
 
     @Override
     protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
@@ -31,40 +32,7 @@ public class MyHandsHander extends DefaultHandshakeHandler{
 
         String id = valuePairMap.get("id").toString();
 
-        MyPrincipal principal = new MyPrincipal();
-        principal.setName(id);
-        return principal;
-    }
 
-
-
-
-    class MyPrincipal implements Principal {
-        private String name;
-
-        @Override
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            MyPrincipal that = (MyPrincipal) o;
-
-            return name != null ? name.equals(that.name) : that.name == null;
-
-        }
-
-        @Override
-        public int hashCode() {
-            return name != null ? name.hashCode() : 0;
-        }
+        return MyPrincipal.builder().name(id).build();
     }
 }
---
+++ src/main/java/com/cxxy/websocket/MyPrincipal.java
@@ -0,0 +1,19 @@
+package com.cxxy.websocket;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.security.Principal;
+
+/**
+ * Author:liuhui
+ * Description:
+ * Date: 6:01 PM 2018/11/26
+ */
+@Data
+@Builder
+public class MyPrincipal implements Principal{
+
+    private String name;
+}
